dc8320474510848a47810ef544076b6de8236baf
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2025-04-19 09:09:08 +0200

    feature: transaction details page

--- src/views/journal-entries/JournalDetailsPage.vue
+++ src/views/journal-entries/JournalDetailsPage.vue
@@ -11,17 +11,23 @@ export default {
     {
         return {
             transaction_id: 0,
+            transactionDetails: null,
         }
     },
     methods: {
-        async getTransactionById()
+        async getTransactionDetailsById()
         {
-
+            await this.http.get(`transactions/details/${this.transaction_id}`).then(res =>
+            {
+                this.transactionDetails = res.data
+                console.log('data', res.data)
+            })
         }
     },
     mounted()
     {
         this.transaction_id = this.$route.params.id;
+        this.getTransactionDetailsById();
     }
 };
 </script>
@@ -29,5 +35,29 @@ export default {
     <Layout>
         <PageHeader title="Joural Details" pageTitle="Utility" />
         <h5>Transaction No #{{ transaction_id }}</h5>
+        <div class="row">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Account Name</th>
+                            <th scope="col">Transaction Type</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(transaction, index) in transactionDetails" :key="index">
+                            <th scope="row">{{ index + 1 }}</th>
+                            <td>{{ transaction.account_name }}</td>
+                            <td>{{ transaction.type }}</td>
+                            <td>{{ transaction.descr }}</td>
+                            <td>{{ transaction.value }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </Layout>
 </template>
